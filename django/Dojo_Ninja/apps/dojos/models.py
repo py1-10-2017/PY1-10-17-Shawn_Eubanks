61ae8b29bb519268c3368ae0fb8149947418d34a
Shawn Eubanks <dev.eube@gmail.com>
2017-11-11 16:20:32 -0600

    Completed Dojo_Ninja and Book_Author Django

---
+++ django/Dojo_Ninja/apps/dojos/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    # Adding new field to Dojo
+    desc = models.TextField(default="")
+
+
+class Ninja(models.Model):
+    dojo_id = models.ForeignKey(Dojo, related_name="ninjas")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
